e8872c9a514bf28be151254435d4d1ca0a39d7ce
laudan_daniel <daniel.laudan@mta.ro>
2020-10-23 09:10:19 +0300

    Adaugare Utilizator,modificare carte
    Detalii
    
    Signed-off-by: laudan_daniel <daniel.laudan@mta.ro>

---
+++ src/Frate.java
@@ -0,0 +1,15 @@
+
+
+public class Frate {
+    public Frate(String name) {
+        Name = name;
+    }
+
+    private String Name;
+
+    public Frate(int sarpe) {
+        Sarpe = sarpe;
+    }
+
+    private int Sarpe;
+}
